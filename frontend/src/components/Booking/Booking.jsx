61da510cae1f4a3cba4b169a300d8d0c7a47a21b
Chandimaisuru <isuruchandima92@gmail.com>
2024-07-04 09:21:35 +0530

    Booking updated

--- frontend/src/components/Booking/Booking.jsx
+++ frontend/src/components/Booking/Booking.jsx
@@ -1,15 +1,21 @@
-import React,{useState} from 'react'
+import React,{useState,useContext} from 'react'
 import './booking.css'
 import { Form, FormGroup, ListGroup, ListGroupItem, Button } from 'reactstrap'
 import { useNavigate } from 'react-router-dom';
-function Booking({ tour, avgRating }) {
+import { BASE_URL } from '../../utils/config';
+import { AuthContext } from '../../context/AuthContext';
 
-    const { price, reviews } = tour;
+function Booking({ tour, avgRating}) {
+
+    const { price, reviews,title  } = tour;
     const navigate = useNavigate();
 
-    const [credentials, setCredentials] = useState({
-        userID: "01", //need to set in backend
-        userEmail: "test@gmail.com",
+    const {user} =useContext(AuthContext)
+
+    const [booking, setBooking] = useState({
+        userID: user && user._id,
+        userEmail: user && user.email,
+        tourName: title,
         fullName: "",
         phone:"",
         guestSize: 1,
@@ -17,21 +23,46 @@ function Booking({ tour, avgRating }) {
       });
 
       const handleChange = (e) => {
-        setCredentials((prev) => ({...prev, [e.target.id]: e.target.value }));
+        setBooking((prev) => ({...prev, [e.target.id]: e.target.value }));
      }
 
 
       const serviceFee = 10;
       const totalAmount =
-        Number(price) * Number(credentials.guestSize) + serviceFee;
+        Number(price) * Number(booking.guestSize) + serviceFee;
 
         //send data to server
 
-         const handleClick = (e) => {
+         const handleClick = async(e) => {
              e.preventDefault();
 
-            navigate("/thank-you");
+             console.log(booking)
 
+             try {
+                if(!user || user== undefined || user== null){
+                    return alert('please login')
+                }
+                const res = await fetch(BASE_URL+"/booking", {
+                    method: "post",
+                    headers: {
+                      "content-type": "application/json",
+                    },
+                    credentials: "include",
+            
+                    body: JSON.stringify(booking),
+                  });
+            
+                  const result = await res.json();
+                  if (!res.ok){
+                  return alert(result.message);
+                  }
+                 
+                  navigate("/thank-you");
+             } catch (error) {
+                alert(error.message)
+             }
+
+          
             }
     
 
